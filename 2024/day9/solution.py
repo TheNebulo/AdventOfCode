e1e6eac223a0c070e4b04777aadfc1a0c2030c92
TheNebulo <amir.djaffar@outlook.com>
2024-12-09 20:12:05 +0100

    Day 9 part 1

---
+++ 2024/day9/solution.py
@@ -0,0 +1,37 @@
+with open('./2024/day9/input.txt', 'r') as file:
+    disk_map = file.readline()
+    
+expanded_disk_map = []
+disk_map_without_space = []
+is_file_block = True
+id = 0
+
+for char in disk_map:
+    amount = int(char)
+    
+    if is_file_block: 
+        for i in range(amount): 
+            expanded_disk_map.append(id)
+            disk_map_without_space.append(id)
+        id += 1
+    else:
+        for i in range(amount): expanded_disk_map.append(None)
+    
+    is_file_block = not is_file_block
+    
+pos = 0
+checksum = 0
+    
+while len(disk_map_without_space) > 0:
+    if expanded_disk_map[0] != None:
+        checksum += (disk_map_without_space[0] * pos)
+        disk_map_without_space.pop(0)
+    
+    else:
+        checksum += (disk_map_without_space[-1] * pos)
+        disk_map_without_space.pop(-1)
+        
+    expanded_disk_map.pop(0)
+    pos += 1
+        
+print(checksum)
