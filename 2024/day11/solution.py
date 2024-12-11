b4a6d3c5b0f4588eb439b500f785f7f0cc2c2548
TheNebulo <amir.djaffar@outlook.com>
2024-12-11 17:00:58 +0100

    Day 11: Removed cache

--- 2024/day11/solution.py
+++ 2024/day11/solution.py
@@ -6,37 +6,22 @@ with open('./2024/day11/input.txt', 'r') as file:
 stones = [int(char) for char in line.split(' ')]
 
 def blink(n, stones):
-    split_cache = {}
-    multiplication_cache = {}
     stones_counter = Counter(stones)
-    
+
     for _ in range(1, n+1):
         new_stones_counter = Counter()
         
         for stone, count in stones_counter.items():
-            if stone == 0: 
-                new_stones_counter[1] += count
-            
-            elif stone in split_cache:
-                new_stones_counter[split_cache[stone][0]] += count
-                new_stones_counter[split_cache[stone][1]] += count
-                
-            elif stone in multiplication_cache: 
-                new_stones_counter[multiplication_cache[stone]] += count
+            if stone == 0: new_stones_counter[1] += count
             
             elif len(str(stone)) % 2 == 0:
                 half_len = len(str(stone))//2
                 first_half = int(str(stone)[:half_len])
                 second_half = int(str(stone)[half_len:])
-                
-                split_cache[stone] = [first_half, second_half]
                 new_stones_counter[first_half] += count
                 new_stones_counter[second_half] += count
             
-            else:
-                new_stone = stone * 2024
-                multiplication_cache[stone] = new_stone
-                new_stones_counter[new_stone] += count
+            else: new_stones_counter[stone * 2024] += count
                 
         stones_counter = new_stones_counter
         
